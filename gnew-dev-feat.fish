85b11f41afd0d42b9a481c138f49b1b001f57c81
Diego Silva <diego.silva@estrelabet.com>
2023-05-29 14:55:42 -0300

    update functions

--- gnew-dev-feat.fish
+++ gnew-dev-feat.fish
@@ -1,6 +1,6 @@
 function gnew-dev-feat --description 'create new feature branch from develop'
   git checkout develop
   git pull origin develop
-  git checkout -b feature/$argv
-  git push --set-upstream origin feature/$argv
+  git checkout -b feat/$argv
+  git push --set-upstream origin feat/$argv
 end
